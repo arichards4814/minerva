b34508524ec1210452b03dfbd0aabdfb2682ff61
arichards4814 <arichards4814@gmail.com>
2020-05-07 10:47:37 -0400

    added tiktok support

--- src/Test.js
+++ src/Test.js
@@ -5,6 +5,7 @@ import Tweet from './ContentContainers/Tweet'
 // redux
 import { connect } from 'react-redux';
 import {  } from './actionCreators'
+import TikTok from './TikTok';
 
 
 
@@ -14,6 +15,7 @@ const Test = props => {
     return (
         <div>
             <Tweet tweet_url="https://twitter.com/IGN/status/1258026570410872835" />
+            <TikTok tiktok_url="https://www.tiktok.com/@imkevinhart/video/6821691366138006789" />
         </div>
     );
 }
---
+++ src/TikTok.js
@@ -0,0 +1,40 @@
+import React, { Component, useState, useEffect } from 'react';
+import './App.css';
+
+// redux
+import { connect } from 'react-redux';
+import {  } from './actionCreators'
+
+
+
+const TikTok = props => {
+    const [content, setContent] = useState({})
+
+    useEffect(()=> {
+        //fetch oEmbed TikTok
+        fetch(`https://www.tiktok.com/oembed?url=${props.tiktok_url}`)
+            .then(resp => resp.json())
+                .then(body => {
+                    setContent(body)
+                })
+    })
+
+    return (
+        <div >
+            {content.html && <div dangerouslySetInnerHTML={{__html: content.html}}></div>}
+        </div>
+    );
+}
+
+
+const mapStateToProps = (state) => {
+    return {
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TikTok);
